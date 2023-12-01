1027d095356bdfed650eb33ba35c5d19151f8660
Tanvirul872 <anmtanvir872@gmail.com>
2023-12-01 12:04:22 +0600

    update

--- prodotti-html-cat.php
+++ prodotti-html-cat.php
@@ -3,6 +3,44 @@
 get_header() ; ?>
 
 
+<!-- banner section start  -->
+
+<section class="productdetails-banner-main" style="background-image:url('https://localhost/mmpozzi/wp-content/themes/bridge-child/assets/images/tv/pro_cat.png')">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="title-box">
+                    <h2>prodotti</h2>
+                    <p>SCEGLI IL PRODOTTO PER LE TUE ESIGENZE</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- banner section end  -->
+
+<!-- breadcrumb section start  -->
+
+<section class="details-breadcrumb-main">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">proditti</a></li>
+                    <li><a href="#">atlas copco</a></li>
+                    <li><a href="#">compressori</a></li>
+                    <li>compressori </li>
+                </ul>
+             
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- breadcrumb section end  -->
 
 
 
