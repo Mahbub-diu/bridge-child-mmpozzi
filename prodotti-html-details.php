6f3f1ce227eb5c052ed2366f9600040cdd6d5c1a
Mahbub-diu <mahbub.mediasoft@gmail.com>
2023-12-01 11:58:11 +0600

    update

--- prodotti-html-details.php
+++ prodotti-html-details.php
@@ -2,50 +2,82 @@
  /* Template Name: Prodotti category details */ 
 get_header() ; ?>
 
- 
 <section class="productdetails-banner-main">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <div class="title-box">
-                    <h2>prodotti</h2>
-                    <p>SCEGLI IL PRODOTTO PER LE TUE ESIGENZE</p>
-                </div>
+   <div class="container">
+      <div class="row">
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="title-box">
+               <h2>prodotti</h2>
+               <p>SCEGLI IL PRODOTTO PER LE TUE ESIGENZE</p>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </section>
 <section class="details-breadcrumb-main">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                <ul>
-                    <li><a href="#">home</a></li>
-                    <li><a href="#">proditti</a></li>
-                    <li><a href="#">atlas copco</a></li>
-                    <li><a href="#">compressori</a></li>
-                    <li>compressori a</li>
-                </ul>
+   <div class="container">
+      <div class="row">
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <ul>
+               <li><a href="#">home</a></li>
+               <li><a href="#">proditti</a></li>
+               <li><a href="#">atlas copco</a></li>
+               <li><a href="#">compressori</a></li>
+               <li>compressori a</li>
+            </ul>
+         </div>
+      </div>
+   </div>
+</section>
+<section class="product-details-main">
+   <div class="product-details-flex">
+      <div class="left-sidebar-main">
+         <div class="content">
+            <div id="jquery-accordion-menu" class="jquery-accordion-menu red">
+           
+               <ul id="demo-list" class="demo-list">
+             
+                  
+                  <li>
+                     <a href="#"> atlas copco</a>
+                     <ul class="submenu">
+                        <!-- <li><a href="#">Web Design </a></li>
+                        <li><a href="#">Hosting </a></li> -->
+                        <li>
+                           <a href="#">compressori </a>
+                           <ul class="submenu submenu-product">
+                              <li><a href="#">compressori 1 </a></li>
+                              <li><a href="#">compressori 2 </a></li>
+                              <li><a href="#">compressori 3 </a></li>
+                              <li><a href="#">compressori 4 </a></li>
+                           </ul> 
+                        </li>
+                        <li><a href="#">Consulting </a></li>
+                     </ul>
+                  </li>
+              
+                  <li>
+                     <a href="#">aria compressa </a>
+                     <ul class="submenu">
+                        <li><a href="#">Web Design </a></li>
+                        <li><a href="#">Graphics </a></li>
+                        <li><a href="#">Photoshop</a></li>
+                        <li><a href="#">Programming </a></li>
+                     </ul>
+                  </li>
+                
+               </ul>
              
             </div>
-        </div>
-    </div>
-</section>
-
-<section class="product-details-main">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi repudiandae voluptate veniam provident ex hic? Non, tempore delectus? Odio quasi corporis dolorum! Officia provident quas id velit maiores neque iure.
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi repudiandae voluptate veniam provident ex hic? Non, tempore delectus? Odio quasi corporis dolorum! Officia provident quas id velit maiores neque iure.
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi repudiandae voluptate veniam provident ex hic? Non, tempore delectus? Odio quasi corporis dolorum! Officia provident quas id velit maiores neque iure.
-            </div>
-        </div>
-    </div>
+         </div>
+      </div>
+      <div class="center-descrption-main">
+         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, placeat laboriosam? Eum iste amet impedit ab, soluta dolore nobis illo, eos sed in aspernatur deserunt suscipit totam provident tenetur distinctio.
+      </div>
+      <div class="right-sidebar-main">
+         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, placeat laboriosam? Eum iste amet impedit ab, soluta dolore nobis illo, eos sed in aspernatur deserunt suscipit totam provident tenetur distinctio.
+      </div>
+   </div>
 </section>
 
 
