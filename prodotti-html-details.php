16cbdc63f40f3b6f5cf7b0181d17f8d6a6a13767
Mahbub-diu <mahbub.mediasoft@gmail.com>
2023-12-01 14:43:39 +0600

    update

--- prodotti-html-details.php
+++ prodotti-html-details.php
@@ -3,116 +3,134 @@
 get_header() ; ?>
 
 <section class="productdetails-banner-main">
-   <div class="container">
-      <div class="row">
-         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="title-box">
-               <h2>prodotti</h2>
-               <p>SCEGLI IL PRODOTTO PER LE TUE ESIGENZE</p>
-            </div>
-         </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <div class="title-box">
+          <h2>prodotti</h2>
+          <p>SCEGLI IL PRODOTTO PER LE TUE ESIGENZE</p>
+        </div>
       </div>
-   </div>
+    </div>
+  </div>
 </section>
 <section class="details-breadcrumb-main">
-   <div class="container">
-      <div class="row">
-         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <ul>
-               <li><a href="#">home</a></li>
-               <li><a href="#">proditti</a></li>
-               <li><a href="#">atlas copco</a></li>
-               <li><a href="#">compressori</a></li>
-               <li>compressori a</li>
-            </ul>
-         </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+        <ul>
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">proditti</a>
+          </li>
+          <li>
+            <a href="#">atlas copco</a>
+          </li>
+          <li>
+            <a href="#">compressori</a>
+          </li>
+          <li>compressori a</li>
+        </ul>
       </div>
-   </div>
+    </div>
+  </div>
 </section>
 <section class="product-details-main">
-   <div class="product-details-flex">
-      <div class="left-sidebar-main">
-         <div class="content">
-            <div id="jquery-accordion-menu" class="jquery-accordion-menu red">
-           
-               <ul id="demo-list" class="demo-list">
-             
-                  
-                  <li>
-                     <a href="#"> atlas copco</a>
-                     <ul class="submenu">
-                        
-                        <li>
-                           <a href="#">compressori </a>
-                           <ul class="submenu">
-                              <li><a href="#" class="active">compressori a </a></li>
-                              <li><a href="#">compressori b </a></li>
-                              <li><a href="#">compressori c </a></li>
-                              <li><a href="#">compressori d </a></li>
-                          
-                           </ul> 
-                        </li>
-                        <li>
-                            <a href="">Essiccatori</a>
-                        </li>
-                        <li>
-                            <a href="">Galleria dell’usato</a>
-                        </li>
-                        <li>
-                            <a href="">Gemeratori d’Azot</a>
-                        </li>
-                        <li>
-                            <a href="">Pompe per Vuoto</a>
-                        </li>
-                        <li>
-                            <a href="">Utensili</a>
-                        </li>
-                        <li>
-                            <a href="">Accessori Reti Aria e Servizi</a>
-                        </li>
-                     </ul>
-                  </li>
-              
-                  <li>
-                     <a href="#">aria compressa </a>
-                     <!-- <ul class="submenu">
-                        <li><a href="#">Web Design </a></li>
-                        <li><a href="#">Graphics </a></li>
-                        <li><a href="#">Photoshop</a></li>
-                        <li><a href="#">Programming </a></li>
-                     </ul> -->
-                  </li>
-                  <li>
-                    <a href="#">MACCHINE UTENSILI</a>
-                  </li>
-                  <li>
-                    <a href="#">UTENSILERIA</a>
-                  </li>
-                  <li>
-                    <a href="#">tornitura ed asportazione </a>
-                  </li>
-                  <li>
-                    <a href="#">aNTINFORTUNISTICA</a>
-                  </li>
-                  <li>
-                    <a href="#">MOTORI</a>
-                  </li>
-                  <li>
-                    <a href="#">ARREDAMENTI </a>
-                  </li>
-                 
-               </ul>
-             
+  <div class="product-details-flex">
+    <div class="left-sidebar-main">
+      <div class="content">
+        <div id="jquery-accordion-menu" class="jquery-accordion-menu red">
+          <ul id="demo-list" class="demo-list">
+            <li>
+              <a href="#"> atlas copco</a>
+              <ul class="submenu">
+                <li>
+                  <a href="#">compressori </a>
+                  <ul class="submenu">
+                    <li>
+                      <a href="#" class="active">compressori a </a>
+                    </li>
+                    <li>
+                      <a href="#">compressori b </a>
+                    </li>
+                    <li>
+                      <a href="#">compressori c </a>
+                    </li>
+                    <li>
+                      <a href="#">compressori d </a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="">Essiccatori</a>
+                </li>
+                <li>
+                  <a href="">Galleria dell’usato</a>
+                </li>
+                <li>
+                  <a href="">Gemeratori d’Azot</a>
+                </li>
+                <li>
+                  <a href="">Pompe per Vuoto</a>
+                </li>
+                <li>
+                  <a href="">Utensili</a>
+                </li>
+                <li>
+                  <a href="">Accessori Reti Aria e Servizi</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">aria compressa </a>
+            </li>
+            <li>
+              <a href="#">MACCHINE UTENSILI</a>
+            </li>
+            <li>
+              <a href="#">UTENSILERIA</a>
+            </li>
+            <li>
+              <a href="#">tornitura ed asportazione </a>
+            </li>
+            <li>
+              <a href="#">aNTINFORTUNISTICA</a>
+            </li>
+            <li>
+              <a href="#">MOTORI</a>
+            </li>
+            <li>
+              <a href="#">ARREDAMENTI </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="center-descrption-main">
+      <div class="center-content-box">
+        <div class="title-box">
+          <h2>compressore A</h2>
+          <p>Lorem ipsum dolor sit amet</p>
+        </div>
+        <div class="image-box">
+          <img src="http://localhost/mmpozzi/wp-content/themes/bridge-child/assets/images/product-details/product-croped.png" alt="">
+        </div>
+        <div class="description">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
+        </div>
+        <div class="related-products">
+            <div class="related-product-flex">
+                <div class="single-image">
+                    <a href="#"></a>
+                </div>
             </div>
-         </div>
+        </div>
       </div>
-      <div class="center-descrption-main">
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, placeat laboriosam? Eum iste amet impedit ab, soluta dolore nobis illo, eos sed in aspernatur deserunt suscipit totam provident tenetur distinctio.
-      </div>
-      <div class="right-sidebar-main">
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, placeat laboriosam? Eum iste amet impedit ab, soluta dolore nobis illo, eos sed in aspernatur deserunt suscipit totam provident tenetur distinctio.
-      </div>
-   </div>
+    </div>
+    <div class="right-sidebar-main"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, placeat laboriosam? Eum iste amet impedit ab, soluta dolore nobis illo, eos sed in aspernatur deserunt suscipit totam provident tenetur distinctio. </div>
+  </div>
 </section>
 
 
